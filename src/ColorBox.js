dce26cac9a364dc6ebfee25a513bc65f5df956f9
Jozef <Jozefw@sbcglobal.net>
2021-08-20 16:40:45 -0700

    first commit

---
+++ src/ColorBox.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import "./ColorBox.css";
+
+class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {copied:false};
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+    changeCopyState(){
+        this.setState({copied:true},()=>{
+            setTimeout(()=>this.setState({copied:false}),1500)
+        })
+    }
+    render() {
+        const {copied} = this.state;
+        const {background,color} = this.props;
+        return (
+            <CopyToClipboard text={color} onCopy={this.changeCopyState}>
+            <div className="ColorBox" style={{background}}>
+                <div style={{background}} className={`copy-overlay ${copied  && "show"} `}></div>
+                    <div className={`copy-mssg ${copied && "show"}`}>
+                        <h1 className="Copied">copied</h1>
+                        <p>{this.props.color}</p>
+                    </div>
+                
+               <div className="copy-container">
+                   <div className="box-content">
+                       <span>{color}</span>
+                   </div>
+                   <button className="copy-button">Copy</button>
+               </div>
+               <span className="see-more">More</span>
+            </div>
+            </CopyToClipboard>
+        )
+    }
+}
+
+export default ColorBox
