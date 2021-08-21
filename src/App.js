420d4cc4fc6293d636ef31f21298baf38ef1be2e
Jozef <Jozefw@sbcglobal.net>
2021-08-21 12:46:46 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Palette from './Palette';
 import SeedColors from './seedColors';
+import React, { Component } from 'react'
+import {generatePalette} from './ColorHelpers';
 
-function App() {
-  return (
-    <div className="App">
+export class App extends Component {
+  render() {
+    console.log(generatePalette(SeedColors[4]));
+    return (
+      <div className="App">
       <Palette {...SeedColors[4]} ></Palette>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
